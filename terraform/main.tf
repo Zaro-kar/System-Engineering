f39e2b5dcc57ee85ca9d1966871f99ecab666d5b
Simon Wimmer <sw250@hdm-stuttgart.de>
2024-12-08 17:12:19 +0100

    initial terraform file

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "webapp-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "main" {
+  name                = "webappacr143101a"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "webapp-aks"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "webapp"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2                     # Anzahl der Nodes im Cluster
+    vm_size    = "Standard_B2ms"       # Größe der virtuellen Maschinen
+  }
+
+  identity {
+    type = "SystemAssigned"            # Verwaltete Identität
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
