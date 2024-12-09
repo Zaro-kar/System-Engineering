9aa350fe3c4798365765b10006ba0fbe321d4682
Simon Wimmer <sw250@hdm-stuttgart.de>
2024-12-09 22:01:41 +0100

    fix: consolidate Terraform backend initialization steps and update provider configuration

--- terraform/backend/backend.tf
+++ terraform/backend/backend.tf
@@ -1,26 +1,14 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "examplestorageaccount"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+    resource_group_name  = "example-resource-group"
+    subscription_id      = "your_subscription_id"
+    tenant_id            = "your_tenant_id"
+  }
+}
+
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-}
-
-resource "azurerm_resource_group" "backend" {
-  name     = "webapp-backend-rg"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "backend" {
-  name                     = "webapptfstate143101a"
-  resource_group_name      = azurerm_resource_group.backend.name
-  location                 = azurerm_resource_group.backend.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "backend" {
-  name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.backend.id
-  container_access_type = "private"
 }
