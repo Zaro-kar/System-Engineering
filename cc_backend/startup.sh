075c2f896b74f48db8cd3e20feec1812c21bb56b
Simon Wimmer <sw250@hdm-stuttgart.de>
2025-01-05 17:09:22 +0100

    fix: replace Gunicorn command with Django runserver in startup script

--- cc_backend/startup.sh
+++ cc_backend/startup.sh
@@ -3,4 +3,5 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Gunicorn-Server starten
-exec gunicorn cc_api.asgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 600
+# exec gunicorn cc_api.asgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 600
+python mange.py runserver 0.0.0.0:8000
