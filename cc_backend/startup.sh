571fe93cd68491c28442189bd0e16258cb5f7326
Simon Wimmer <sw250@hdm-stuttgart.de>
2025-01-05 17:25:17 +0100

    fix: correct typo in startup script to use 'manage.py' for Django runserver

--- cc_backend/startup.sh
+++ cc_backend/startup.sh
@@ -4,4 +4,4 @@ python manage.py migrate
 
 # Gunicorn-Server starten
 # exec gunicorn cc_api.asgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 600
-python mange.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
