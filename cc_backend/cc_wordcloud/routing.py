30e9daa115dbbd54b5188f64f27d32c535fdf139
milenaKuebler <milli1999@gmx.de>
2025-02-12 23:27:07 +0100

    build: add api prefix to ws route

--- cc_backend/cc_wordcloud/routing.py
+++ cc_backend/cc_wordcloud/routing.py
@@ -7,5 +7,5 @@ from django.urls import path
 from cc_wordcloud.consumer import WordsUpdateConsumer
 
 websocket_urlpatterns = [
-    path('ws/sessions/<str:session_id>/', WordsUpdateConsumer.as_asgi()),
+    path('api/ws/sessions/<str:session_id>/', WordsUpdateConsumer.as_asgi()),
 ]
