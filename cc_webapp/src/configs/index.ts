e95018803f2f13d8a18ecf93443c1e67687e1da5
milenaKuebler <milli1999@gmx.de>
2025-02-12 20:34:09 +0100

    build: add use regex annotation

--- cc_webapp/src/configs/index.ts
+++ cc_webapp/src/configs/index.ts
@@ -1,5 +1,5 @@
 export const API_URL = import.meta.env.VITE_APP_API_URL || 'http://50.85.88.155/api';
 
-export const WS_URL = import.meta.env.VITE_APP_WS_URL || 'ws://50.85.88.155/api';
+export const WS_URL = import.meta.env.VITE_APP_WS_URL || 'http://50.85.88.155/api';
 
 export const FRONTEND_URL = import.meta.env.VITE_APP_FRONTEND_URL || 'http://50.85.88.155';
